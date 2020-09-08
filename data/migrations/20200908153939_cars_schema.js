4bc8ca2a24dad7548930d0f1c905bae21434ef7b
royeradames <royeraadames@gmail.com>
2020-09-08 15:46:42 -0400

    cars schema migration completed

--- data/migrations/20200908153939_cars_schema.js
+++ data/migrations/20200908153939_cars_schema.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex) {
-  
-};
+    return knex.schema.createTable('cars', function (table) {
+        table.increments()
+        table.string('VIN').index().notNullable().unique()
+        table.string('make').notNullable()
+        table.string('model').notNullable()
+        table.string('mileage').notNullable()
+        table.string('transmissionType')
+        table.string('titleStatus')
+        table.timestamps()
+      })
+}
 
 exports.down = function(knex) {
-  
-};
+    return knex.schema.dropTableIfExists('cars')
+}

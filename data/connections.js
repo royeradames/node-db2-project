edfba669564fc1e0ad6419e2e62ded31883fc3af
royeradames <royeraadames@gmail.com>
2020-09-08 15:35:30 -0400

    completed connnections file

--- data/connections.js
+++ data/connections.js
@@ -1,2 +1,12 @@
 // importing library
-const knex =require('knex')
+const knex = require('knex')
+
+// importing files
+const knexfile = require('../knexfile')
+
+//applying the development configuration dynamically
+const environment = process.env.NODE_ENV || 'development'
+
+const config = knexfile[environment]
+
+module.exports = knex(config)
